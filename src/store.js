f4867dd3b6e3b572b4394dd2016cd6fe96f66860
serhatbek <serhat.bekk@gmail.com>
2022-09-19 22:20:30 +0300

    store

---
+++ src/store.js
@@ -0,0 +1,9 @@
+import { configureStore } from '@reduxjs/toolkit';
+
+export const store = configureStore({
+  reducer: {},
+});
+
+/**
+ ** [store] ➡ Store uygulamının mevcut tüm state'ini oluşturur.
+ */
