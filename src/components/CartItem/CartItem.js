265fdf76a2138c4999ff162c126e2fa820271261
serhatbek <serhat.bekk@gmail.com>
2022-09-22 21:42:04 +0300

    cart item

--- src/components/CartItem/CartItem.js
+++ src/components/CartItem/CartItem.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import { ChevronDown, ChevronUp } from '../../icons';
 
-const CartItem = () => {
-  return <div>CartItem</div>;
+const CartItem = ({ id, img, price, title, amount }) => {
+  return (
+    <article className='cart-item'>
+      <img src={img} alt={title} />
+      <div>
+        <h4>{title}</h4>
+        <h4 className='item-price'>${price}</h4>
+        <button className='remove-btn'>remove</button>
+      </div>
+      <div>
+        <button className='amount-btn'>
+          <ChevronUp />
+        </button>
+        <div className='amount'>{amount}</div>
+        <button className='amount-btn'>
+          <ChevronDown />
+        </button>
+      </div>
+    </article>
+  );
 };
 
 export default CartItem;
