a4653b3826282c404bb8deb7b7989b72676b4e5c
serhatbek <serhat.bekk@gmail.com>
2022-09-24 22:14:40 +0300

    Calculate Totals

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import './App.css';
+import { useEffect } from 'react';
+import { calculateTotals } from './features/cart/cartSlice';
+import { useDispatch, useSelector } from 'react-redux';
+
 import Navbar from './components/Navbar/Navbar';
 import CartContainer from './components/CartContainer/CartContainer';
 
 function App() {
+  const dispatch = useDispatch();
+  const { cartItems } = useSelector((store) => store.cart);
+
+  useEffect(() => {
+    dispatch(calculateTotals());
+  }, [cartItems]);
   return (
     <main>
       <Navbar />
--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -3,7 +3,7 @@ import cartItems from '../../cartItems';
 
 const initialState = {
   cartItems: cartItems,
-  amount: 1,
+  amount: 0,
   total: 0,
   isLoading: true,
 };
@@ -11,6 +11,7 @@ const initialState = {
 const cartSlice = createSlice({
   name: 'cart',
   initialState,
+
   reducers: {
     clearCart: (state) => {
       state.cartItems = [];
@@ -25,14 +26,31 @@ const cartSlice = createSlice({
       const cartItem = state.cartItems.find((item) => item.id === payload);
       cartItem.amount = cartItem.amount + 1;
     },
+
     decreaseAmount: (state, { payload }) => {
       const cartItem = state.cartItems.find((item) => item.id === payload);
       cartItem.amount = cartItem.amount - 1;
     },
+
+    calculateTotals: (state) => {
+      let amount = 0;
+      let total = 0;
+      state.cartItems.forEach((item) => {
+        amount += item.amount;
+        total += item.amount * item.price;
+      });
+      state.amount = amount;
+      state.total = total;
+    },
   },
 });
 
-export const { clearCart, increaseAmount, decreaseAmount, removeItem } =
-  cartSlice.actions;
+export const {
+  clearCart,
+  increaseAmount,
+  decreaseAmount,
+  removeItem,
+  calculateTotals,
+} = cartSlice.actions;
 
 export default cartSlice.reducer;
